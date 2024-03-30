fa432fcde91582c838046aa642ae48661840a8b8
sluger <lug.stefan@gmail.com>
2024-03-30 18:40:29 +0100

    Add DTO

--- Program.cs
+++ Program.cs
@@ -41,38 +41,46 @@ app.Run();
 
 static async Task<IResult> GetAllTodos(TodoDbContext db)
 {
-    return TypedResults.Ok(await db.Todos.ToArrayAsync());
+    return TypedResults.Ok(await db.Todos.Select(x => new TodoItemDTO(x)).ToArrayAsync());
 }
 
 static async Task<IResult> GetCompleteTodos(TodoDbContext db)
 {
-    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
+    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).Select(x => new TodoItemDTO(x)).ToListAsync());
 }
 
 static async Task<IResult> GetTodo(int id, TodoDbContext db)
 {
     return await db.Todos.FindAsync(id)
         is Todo todo
-            ? TypedResults.Ok(todo)
+            ? TypedResults.Ok(new TodoItemDTO(todo))
             : TypedResults.NotFound();
 }
 
-static async Task<IResult> CreateTodo(Todo todo, TodoDbContext db)
+static async Task<IResult> CreateTodo(TodoItemDTO todoItemDTO, TodoDbContext db)
 {
-    db.Todos.Add(todo);
+    var todoItem = new Todo
+    {
+        IsComplete = todoItemDTO.IsComplete,
+        Name = todoItemDTO.Name
+    };
+
+    db.Todos.Add(todoItem);
     await db.SaveChangesAsync();
 
-    return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+    todoItemDTO = new TodoItemDTO(todoItem);
+
+    return TypedResults.Created($"/todoitems/{todoItem.Id}", todoItemDTO);
 }
 
-static async Task<IResult> UpdateTodo(int id, Todo inputTodo, TodoDbContext db)
+static async Task<IResult> UpdateTodo(int id, TodoItemDTO todoItemDTO, TodoDbContext db)
 {
     var todo = await db.Todos.FindAsync(id);
 
     if (todo is null) return TypedResults.NotFound();
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+    todo.Name = todoItemDTO.Name;
+    todo.IsComplete = todoItemDTO.IsComplete;
 
     await db.SaveChangesAsync();
 
--- Todo.cs
+++ Todo.cs
@@ -3,4 +3,6 @@ public class Todo
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+
+    public string? secret { get; set; }
 }
